be19bdb512150d314098c611f06371378b775de7
Russell Steadman <russellsteadman@gmail.com>
2017-08-04 08:59:46 -0400

    Added autoprefixer

---
+++ college/showcase/gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+ 
+gulp.task('default', () =>
+    gulp.src('prestyle.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', "> 1% in US"]
+        }))
+        .pipe(rename({
+            basename: "style"
+        }))
+        .pipe(gulp.dest(''))
+);
